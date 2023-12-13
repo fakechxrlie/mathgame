aa837901c10237750be3ab677f0d4a9dcadb8f4a
fakechxrlie <153480321+fakechxrlie@users.noreply.github.com>
2023-12-12 23:50:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,78 +1,79 @@
-let problems = []; // Array to store problems and options
-let currentProblemIndex = 0;
+const questions = [];
+let currentQuestionIndex = 0;
 let startTime, endTime;
 
-// Fetch problems from problems.txt
-fetch('problems.txt')
-  .then(response => response.text())
-  .then(data => {
-    problems = data.split('\n').map(problem => {
-      const [question, ...options] = problem.split(',');
-      const answer = options[0]; // First option is the correct answer
-      return { question, options, answer };
-    });
-  });
+function startGame() {
+  document.getElementById('startBtn').style.display = 'none';
+  document.getElementById('result').innerHTML = '';
+  document.getElementById('choices').innerHTML = '';
 
-function displayProblem() {
-  const currentProblem = problems[currentProblemIndex];
-  document.getElementById('problem').innerText = `Solve: ${currentProblem.question}`;
-
-  const options = currentProblem.options.slice(1); // Exclude the correct answer from options
-  options.push(currentProblem.answer); // Add correct answer to options
-  const shuffledOptions = shuffleArray([...options]); // Shuffle options
-  const optionsContainer = document.getElementById('options');
-  optionsContainer.innerHTML = '';
-
-  shuffledOptions.forEach(option => {
-    const button = document.createElement('button');
-    button.textContent = option;
-    button.classList.add('option-btn');
-    button.addEventListener('click', () => checkAnswer(option));
-    optionsContainer.appendChild(button);
-  });
-}
-
-function startTimer() {
+  generateQuestions();
+  displayQuestion();
   startTime = new Date().getTime();
 }
 
-function stopTimer() {
-  endTime = new Date().getTime();
+function generateQuestions() {
+  for (let i = 0; i < 5; i++) {
+    const num1 = Math.floor(Math.random() * 10);
+    const num2 = Math.floor(Math.random() * 10);
+    const answer = num1 + num2;
+    const choices = generateChoices(answer);
+    const question = {
+      question: `${num1} + ${num2} = ?`,
+      choices: choices,
+      correctAnswer: answer
+    };
+    questions.push(question);
+  }
 }
 
-function checkAnswer(selectedAnswer) {
-  stopTimer();
-  const elapsedTime = (endTime - startTime) / 1000; // Convert to seconds
-  document.getElementById('time').innerText = `Time: ${elapsedTime.toFixed(2)}s`;
+function generateChoices(answer) {
+  const choices = [];
+  choices.push(answer);
 
-  const correctAnswer = problems[currentProblemIndex].answer;
-  if (selectedAnswer === correctAnswer) {
-    document.getElementById('result').innerText = 'Correct!';
-  } else {
-    document.getElementById('result').innerText = 'Incorrect. Try again.';
+  while (choices.length < 4) {
+    const random = Math.floor(Math.random() * 20);
+    if (!choices.includes(random) && random !== answer) {
+      choices.push(random);
+    }
   }
 
-  setTimeout(() => {
-    document.getElementById('result').innerText = '';
-    document.getElementById('time').innerText = 'Time: 0s';
-    currentProblemIndex = (currentProblemIndex + 1) % problems.length; // Move to the next problem
-    displayProblem();
-    startTimer();
-  }, 2000); // Reset after 2 seconds
+  return shuffleArray(choices);
 }
 
-function startGame() {
-  document.querySelector('.start-btn').style.display = 'none';
-  document.getElementById('game').style.display = 'block';
-  displayProblem();
-  startTimer();
-}
-
-// Function to shuffle array elements
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
+}
+
+function displayQuestion() {
+  if (currentQuestionIndex < questions.length) {
+    const question = questions[currentQuestionIndex];
+    document.getElementById('question').textContent = question.question;
+
+    question.choices.forEach((choice, index) => {
+      const button = document.createElement('button');
+      button.textContent = choice;
+      button.onclick = () => checkAnswer(choice, question.correctAnswer);
+      document.getElementById('choices').appendChild(button);
+    });
+  } else {
+    endTime = new Date().getTime();
+    const totalTime = (endTime - startTime) / 1000;
+    document.getElementById('timer').textContent = `Time: ${totalTime.toFixed(1)}s`;
+    document.getElementById('result').textContent = 'Game Over';
+  }
+}
+
+function checkAnswer(selectedAnswer, correctAnswer) {
+  if (parseInt(selectedAnswer) === correctAnswer) {
+    document.getElementById('result').textContent = 'Correct!';
+  } else {
+    document.getElementById('result').textContent = 'Wrong!';
+  }
+  currentQuestionIndex++;
+  setTimeout(displayQuestion, 1000);
 }
